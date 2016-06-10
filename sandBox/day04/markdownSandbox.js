56b7d699d12e0d844de1d1f70d3106ff573de0a6
Michelle Sri <michelle.sri@gmail.com>
2016-06-10 11:02:08 -0700

    edited markdown stuff to update with stuff learned in lecture

--- sandBox/day04/markdownSandbox.js
+++ sandBox/day04/markdownSandbox.js
@@ -1,15 +1,28 @@
+// this function runs on load
 $(function(){
-  var $textB = $('#textB');
-  var $pMarkOut = $('#pMarkOut');
+  // gets the element with id input. this is on the textarea tag
+  var $input = $('#input');
+  // get the element with id output
+  var $output = $('#output');
 
+// create a function that
   function render() {
-    var textVal = $textB.val();
-    var m = marked(textVal);
+    // takes in the value that the user types in textarea
+    var inputVal = $input.val();
 
-    $pMarkOut.html(m);
-    $pMarkOut.find('pre code').each(function(i, block){
+    // run the value that the user inputs through markdown filter and stores the result in the m variable.
+    var m = marked(inputVal);
+
+// put the m variable text into the output box which comes from the element with id of output
+    $output.html(m);
+    $output.find('pre code').each(function(i, block){
       hljs.highlightBlock(block);
+      console.log('i = ' + i);
+      // i = the number of times it finds something that needs to be highlighted
+      console.log('block = ' + block);
+      // the actual code that will get highlighted
     });
   }
-  $textB.on('input', render);
+  // when someone does the input event inside of the textarea with id of input, call the render function.
+  $input.on('input', render);
 });
