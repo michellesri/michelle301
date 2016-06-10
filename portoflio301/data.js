f23b18934f6aafa53e6fcb41dd404a7144ae5689
Michelle Sri <michelle.sri@gmail.com>
2016-06-09 23:00:06 -0700

    started over with n with new nav bar

---
+++ portoflio301/data.js
@@ -0,0 +1,14 @@
+var projectData = [
+  {
+    name: 'basketball',
+    location: 'gym',
+    experience: '13 years',
+    gear: 'shoes, jersey, shorts'
+  },
+  {
+    name: 'Dance',
+    location: 'studio',
+    experience: '2 years',
+    gear: 'speakers, earphones, sweatpants'
+  }
+];
