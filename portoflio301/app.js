207b2d250a8b1372fe030b86ef70890980b37443
Michelle Sri <michelle.sri@gmail.com>
2016-06-15 14:40:04 -0700

    trying to fix the .clone

--- portoflio301/app.js
+++ portoflio301/app.js
@@ -3,15 +3,15 @@ var projects = [];
 
 var articleView = {};
 
-function Project(activity) {
-  this.name = activity.name;
-  this.location = activity.location;
-  this.experience = activity.experience;
-  this.gear = activity.gear;
+function Project(name, location, experience, gear) {
+  this.name = name;
+  this.location = location;
+  this.experience = experience;
+  this.gear = gear;
 }
 
 Project.prototype.toHtml = function(){
-  var $newProject = $('div.projects').clone();
+  var $newProject = $('.projects').clone();
   $newProject.removeClass('projects');
   $newProject.find('h2').text(this.name);
   $newProject.find('#one').text(this.location);
@@ -27,10 +27,15 @@ projectData.forEach(function(ele){
 
 projects.forEach(function(a){
   $('#projectDiv').append(a.toHtml());
-  console.log(a.toHtml().html());
+  console.log(a.toHtml());
 });
 
-$('.projects').remove();
+// $('.projects').remove();
+
+// trying to create a new project and have it appear on the page, but it is not working
+var swimming = new Project('swim','pool','5 years','swimsuit');
+console.log(swimming);
+projectData.push(swimming);
 
 articleView.populateFilters = function() {
   for (var i = 0; i < projectData.length; i++){
@@ -39,22 +44,37 @@ articleView.populateFilters = function() {
     if ($('#filter1 option[value="' + experience + '"]').length === 0){
       $('#filter1').append(experienceTag);
     }
-  }
+
+    var location = projectData[i].location;
+    var locationTag = '<option value="' + location + '" >' + location + '</option>';
+    if ($('#filter2 option[value="' + location + '"]').length === 0){
+      $('#filter2').append(locationTag);
+    }
+  };
 };
 
-
 articleView.populateFilters();
-// trying to create a new project and have it appear on the page, but it is not working
-var swimming = new Project('water','pool','5 years','swimsuit');
-projectData.push(swimming);
 
-function handleMainNav() {
-  $('.main-nav').on('click', '.tab', function(e){
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
+articleView.handleAuthorFilter = function() {
+  $('#filter1').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
   });
-  $('.main-nav .tab:first').click();
-
-}
-
-handleMainNav();
+};
+//
+// function handleMainNav() {
+//   $('.main-nav').on('click', '.tab', function(e){
+//     $('.tab-content').hide();
+//     $('#' + $(this).data('content')).fadeIn();
+//   });
+//   $('.main-nav .tab:first').click();
+//
+// }
+//
+// handleMainNav();
